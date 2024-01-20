f8ef7c17c983be00cb87df7c953aacef23d8de33
Vitaliy <mbiwapa72@gmail.com>
2024-01-20 15:38:35 +0300

    iter12 fix vet

--- internal/agent/sender/sender.go
+++ internal/agent/sender/sender.go
@@ -29,40 +29,17 @@ func Start(stor AllMetricGeter, sender MetricSender, reportInterval int64, logge
 				"Cant get all metrics")
 			panic("Stor unavailable!")
 		}
-		// for _, metric := range gauge {
-
-		// 	if metric[0] != "" && metric[1] != "" {
-		// 		err = sender.Send(format.Gauge, metric[0], metric[1])
-		// 		if err != nil {
-		// 			//TODO error chanel
-		// 			logger.Error(
-		// 				"Cant send metric",
-		// 				zap.String("type", format.Gauge),
-		// 				zap.String("name", metric[0]),
-		// 				zap.String("value", metric[1]),
-		// 				zap.Error(err))
-		// 			panic(err.Error())
-
-		// 		}
-		// 	}
-		// }
-		// for _, metric := range counter {
-
-		// 	if metric[0] != "" && metric[1] != "" {
-		// 		err = sender.Send(format.Counter, metric[0], metric[1])
-		// 		if err != nil {
-		// 			//TODO error chanel
-		// 			logger.Error(
-		// 				"Cant send metric",
-		// 				zap.String("type", format.Counter),
-		// 				zap.String("name", metric[0]),
-		// 				zap.String("value", metric[1]),
-		// 				zap.Error(err))
-		// 			panic(err.Error())
-		// 		}
-		// 	}
-		// }
 		err = sender.Send(gauge, counter)
+		if err != nil {
+			//TODO error chanel
+			logger.Error(
+				"Cant send metric",
+				zap.Any("gauge", gauge),
+				zap.Any("counter", counter),
+				zap.Error(err))
+			panic(err.Error())
+
+		}
 		sleepSecond := time.Duration(reportInterval) * time.Second
 		time.Sleep(sleepSecond)
 	}
